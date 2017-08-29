db838a992dfc25aff458519f7779126aeb64dba0
Peter Shon <n/a>
2017-08-29 16:43:37 -0400

    testing sql connection

--- ProjectTest/UnitTest1.cs
+++ ProjectTest/UnitTest1.cs
@@ -1,11 +1,17 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Configuration;
 
 namespace ProjectTest {
 	[TestClass]
 	public class UnitTest1 {
 
 		[TestMethod]
-		public void TestMethod1() { }
+		public void TestMethod1() {
+			var cs = ConfigurationManager.ConnectionStrings["fantasy"].ConnectionString;
+			string sql = "SELECT * FROM ff.Player";
+
+			using (var conn = cs.conne)
+		}
 
 	}
 }
