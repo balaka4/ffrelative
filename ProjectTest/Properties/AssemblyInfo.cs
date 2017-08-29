651674cf3b35e021716506825e63873b95938139
Peter Shon <n/a>
2017-08-29 16:36:05 -0400

    test project

--- ProjectTest/Properties/AssemblyInfo.cs
+++ ProjectTest/Properties/AssemblyInfo.cs
@@ -2,18 +2,18 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("StatsLoader")]
+[assembly: AssemblyTitle("ProjectTest")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("StatsLoader")]
+[assembly: AssemblyProduct("ProjectTest")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 [assembly: ComVisible(false)]
 
-[assembly: Guid("fc2fd6a2-c2f6-41b5-ac95-8a1e5a475eb2")]
+[assembly: Guid("ea09cb20-8cc4-494e-831a-8964f12dd3c9")]
 
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
--- ProjectTest/UnitTest1.cs
+++ ProjectTest/UnitTest1.cs
@@ -1,6 +1,6 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-namespace StatsLoader {
+namespace ProjectTest {
 	[TestClass]
 	public class UnitTest1 {
 
