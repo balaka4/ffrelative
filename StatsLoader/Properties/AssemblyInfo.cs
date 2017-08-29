8f8760ce72ea4c9288d9ca45e40073c0e1c47730
Peter Shon <n/a>
2017-08-29 16:11:16 -0400

    stats loader test

---
+++ StatsLoader/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("StatsLoader")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("StatsLoader")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("fc2fd6a2-c2f6-41b5-ac95-8a1e5a475eb2")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ StatsLoader/UnitTest1.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace StatsLoader {
+	[TestClass]
+	public class UnitTest1 {
+
+		[TestMethod]
+		public void TestMethod1() { }
+
+	}
+}
