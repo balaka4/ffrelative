9892b4aade5a4342d2f32c073b0837eabb9f82ae
Peter Shon <n/a>
2017-08-29 17:04:45 -0400

    stats projection table

--- Sql/ff_player.sql
+++ Sql/ff_player.sql
@@ -1,6 +1,7 @@
 use fantasy
 go
 
+drop table if exists ff.StatsProjection
 drop table if exists ff.Player 
 
 create table ff.Player (
@@ -11,3 +12,28 @@ create table ff.Player (
 	constraint pk_ff_player primary key nonclustered (player_id)
 )
 go
+
+create table ff.StatsProjection (
+	player_id		bigint			not null,
+	[source]		varchar(100)	not null,
+
+	pass_yards		decimal(32, 10)	not null default 0,
+	pass_td			decimal(32, 10)	not null default 0,
+	pass_int		decimal(32, 10)	not null default 0,
+	rush_yards		decimal(32, 10)	not null default 0,
+	rush_td			decimal(32, 10)	not null default 0,
+	rec_yards		decimal(32, 10)	not null default 0,
+	rec_td			decimal(32, 10)	not null default 0,
+	fg_made			decimal(32, 10)	not null default 0,
+	fg_att			decimal(32, 10)	not null default 0,
+	xp_made			decimal(32, 10)	not null default 0,
+	xp_att			decimal(32, 10)	not null default 0,
+	sacks			decimal(32, 10)	not null default 0,
+	interceptions	decimal(32, 10)	not null default 0,
+	fum_rec			decimal(32, 10)	not null default 0,
+	safeties		decimal(32, 10)	not null default 0,
+	def_td			decimal(32, 10)	not null default 0,
+	
+	constraint pk_ff_statsproj primary key nonclustered (player_id, [source]),
+	constraint fk_ff_statsproj_playerid foreign key (player_id) references ff.Player (player_id)
+)
